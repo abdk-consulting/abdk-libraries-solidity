d20794314997f5209c8995e337142fb5c0466517
Mikhail Vladimirov <mikhail.vladimirov@gmail.com>
2020-06-22 14:24:25 +0300

    Compiler version requirements updated, license moved to a separate file

--- ABDKMath64x64.sol
+++ ABDKMath64x64.sol
@@ -2,7 +2,7 @@
  * ABDK Math 64.64 Smart Contract Library.  Copyright © 2019 by ABDK Consulting.
  * Author: Mikhail Vladimirov <mikhail.vladimirov@gmail.com>
  */
-pragma solidity ^0.5.7;
+pragma solidity ^0.5.0 || ^0.6.0;
 
 /**
  * Smart contract library of mathematical functions operating with signed
